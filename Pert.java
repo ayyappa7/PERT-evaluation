f638d35e024f7dbcfe0d80f89278042378b2fe7b
ayyappa7 <ayyappa715@gmail.com>
2017-07-26 21:46:15 +0530

    Main Class

---
+++ Pert.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pert;
+import part.*;
+/**
+ *
+ * @author Ayyappa
+ */
+public class Pert {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        home h1=new home();
+        
+    }
+}
